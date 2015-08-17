f1e7fadf01d7757b1c0cc90e831ce638a05930e3
Jyoti Singh <jyotisingh7@gmail.com>
2015-08-17 15:24:07 +0530

    applying optimizations to 15.2.0 user docs

--- user/15.2.0/gitbook/plugins/gitbook-plugin-mathjax/plugin.js
+++ user/15.2.0/gitbook/plugins/gitbook-plugin-mathjax/plugin.js
@@ -1,13 +1,1 @@
-require(["gitbook"], function(gitbook) {
-    MathJax.Hub.Config({
-        tex2jax: {
-            processEscapes: true
-        }
-    });
-
-
-    gitbook.events.bind("page.change", function() {
-        MathJax.Hub.Typeset()
-    });
-});
-
+require(["gitbook"],function(a){MathJax.Hub.Config({tex2jax:{processEscapes:!0}}),a.events.bind("page.change",function(){MathJax.Hub.Typeset()})});
